0a5e074b7739462b283df9a7f12abab857056644
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-04-08 10:34:56 +0200

    tagitbort count

--- frontendProject/src/App.jsx
+++ frontendProject/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return <></>;
+  return <></>;
 }
 
 export default App;
