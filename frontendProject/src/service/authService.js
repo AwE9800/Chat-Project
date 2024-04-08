c41fe9163939156d2bc027a760ea1568d2a158e3
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-04-08 09:45:11 +0200

    registerUser was created

--- frontendProject/src/service/authService.js
+++ frontendProject/src/service/authService.js
@@ -1,16 +1,38 @@
-import { sendData } from '../util/requests.js';
+import { sendData } from "../util/requests.js";
 
 async function authenticate(credentials) {
-    const resp = await sendData('http://localhost:5000/auth/login', credentials);
+  const resp = await sendData("http://localhost:5000/auth/login", credentials);
 
-    if (resp.status === 200) {
-        const { token } = await resp.json();
-        sessionStorage.setItem('token', token);
+  if (resp.status === 200) {
+    const { token } = await resp.json();
+    sessionStorage.setItem("token", token);
 
-        return { authenticated: true };
-    } else {
-        return { authenticated: false };
-    }
+    return { authenticated: true };
+  } else {
+    return { authenticated: false };
+  }
 }
 
 export { authenticate };
+
+async function registerUser(userData) {
+  try {
+    const response = await sendData(
+      "http://localhost:5000/auth/register",
+      userData
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      return { success: true, message: "Registration successful" };
+    } else {
+      const errorData = await response.json();
+      return { success: false, message: errorData.err };
+    }
+  } catch (error) {
+    console.error("Error registering user:", error);
+    return { success: false, message: "Internal server error" };
+  }
+}
+
+export { registerUser };
