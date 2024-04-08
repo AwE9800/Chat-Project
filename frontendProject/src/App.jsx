6e64bf28dcb942af5b56820fad648ab534a45dea
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-04-08 11:14:09 +0200

    har försökt inplentera register i app.jsx

--- frontendProject/src/App.jsx
+++ frontendProject/src/App.jsx
@@ -1,20 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Login from './login/login';
-function App() {
-    const token = sessionStorage.getItem('token');
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./login/login";
+import Register from "./service/register/register";
 
-    return (
-        <>
-            <Router>
-                <main>
-                    <Routes>
-                        <Route path="/login" element={token ? alert('välkommen') : <Login />} />
-                    </Routes>
-                </main>
-            </Router>
-        </>
-    );
+function App() {
+  const token = sessionStorage.getItem("token");
+
+  return (
+    <>
+      <Router>
+        <main>
+          <Routes>
+            <Route
+              path="/login"
+              element={token ? alert("välkommen") : <Login />}
+            />
+            <Route
+              path="/register"
+              element={token ? alert("du är nu registrerad") : <Register />}
+            />
+          </Routes>
+        </main>
+      </Router>
+    </>
+  );
 }
 
 export default App;
