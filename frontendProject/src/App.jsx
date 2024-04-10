acfa3dab010a3a26533581ced26da2666e9848b7
AwE9800 <143619434+AwE9800@users.noreply.github.com>
2024-04-10 08:24:56 +0200

    changes

--- frontendProject/src/App.jsx
+++ frontendProject/src/App.jsx
@@ -1,30 +1,25 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from './login/Login';
+import Login from './login/login';
 import FirstPage from './firstPage/FirstPage';
-import Register from "./register/register";
-
+import Register from './register/register';
 
 function App() {
-  const token = sessionStorage.getItem("token");
+    const token = sessionStorage.getItem('token');
 
-  return (
-    <>
-      <Router>
-        <main>
-          <Routes>
-            <Route path="/" element={<FirstPage />} />  
-            <Route
-              path="/login"
-              element={token ? alert("välkommen") : <Login />}
-            />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </main>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<FirstPage />} />
+                        <Route path="/login" element={token ? alert('välkommen') : <Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </Routes>
+                </main>
+            </Router>
+        </>
+    );
 }
 
 export default App;
